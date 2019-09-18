8f087ebdac4a092e71b003e0fc94732b1fcb0401
Guilherme Alonso <guilherme.malonso@gmail.com>
2019-09-17 22:58:47 -0300

    Initial commit

---
+++ src/app/validators/SessionStore.js
@@ -0,0 +1,21 @@
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string().required(),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ error: 'Validation fails', messages: err.inner });
+  }
+};
